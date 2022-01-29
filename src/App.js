94f431aa709cca8ee5ab5a57deffae63ac44c4ae
Aayush <falcon.bridge2000@gmail.com>
2022-01-30 04:55:07 +0530

    used the other state slice in the required components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Fragment } from "react";
 
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {isAuth && <UserProfile />}
+      {!isAuth && <Auth />}
       <Counter />
     </Fragment>
   );
